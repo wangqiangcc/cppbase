5b5e3f751b020c8db916cae699f78b3f93f50350
dnybz <cnfreebsd@163.com>
2020-09-17 17:58:33 +0800

    remove temp file suffix

--- src/base/files/file_util_posix.cc
+++ src/base/files/file_util_posix.cc
@@ -135,11 +135,7 @@ std::string TempFileName() {
   return StringPrintf(".%s.XXXXXX", base::mac::BaseBundleID());
 #endif
 
-#if defined(GOOGLE_CHROME_BUILD)
-  return std::string(".com.google.Chrome.XXXXXX");
-#else
-  return std::string(".org.chromium.Chromium.XXXXXX");
-#endif
+  return std::string(".XXXXXX");
 }
 
 // Creates and opens a temporary file in |directory|, returning the
